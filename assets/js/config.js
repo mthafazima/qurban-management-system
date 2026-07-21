8b92af8978f6b50b92ee604e85bd3655092a8816
mthafazima <majelishafazima@gmail.com>
2026-07-21 07:24:54 +0700

    Initial commit

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@
  * Cara ambil: buka project Apps Script -> Deploy -> Manage deployments
  * -> Web app -> copy "Web app URL" (yang berakhiran /exec).
  */
-const API_URL = "https://script.google.com/macros/s/GANTI_DENGAN_DEPLOYMENT_ID_KAMU/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzntFH9Crj2lI_Z7KQ6C1gxMJMlLln8QVFLQ_adIwvkw7cv502FhoSxhWC5kJ0S8q_s/exec";
 
 /**
  * Logo aplikasi / Super Admin. Dipakai di halaman login, dan di sidebar
